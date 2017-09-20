3b93679c0c774a1460681e9eb5d83ab67f3b011f
Christopher Guan <cwguan@ucsd.edu>
2017-09-20 13:30:54 -0700

    another typo

--- organizer.py
+++ organizer.py
@@ -36,7 +36,7 @@ def renderCreate_Result():
 
     return redirect(url_for('renderCreate2'))
 
-@app.route('/create2', methods=['GET', 'Post'])
+@app.route('/create2', methods=['GET', 'POST'])
 def renderCreate2():
     return render_template('create2.html')
 
