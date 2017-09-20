ddc252b9b6d447656456ad60194cb8a86f67f2ed
Christopher Guan <cwguan@ucsd.edu>
2017-09-19 17:14:07 -0700

    Removed comments for mLab & mongoDB

--- organizer.py
+++ organizer.py
@@ -9,9 +9,8 @@ import traceback
 
 app = Flask(__name__)
 app.debug = False
-#app.secret_key = os.environ['APP_SECRET_KEY']
+app.secret_key = os.environ['APP_SECRET_KEY']
 
-'''
 # Configures correct environmental variables for MongoDB with mLab
 app.config['MONGO_HOST'] = os.environ['MONGO_HOST']
 app.config['MONGO_PORT'] = int(os.environ['MONGO_PORT'])
@@ -19,7 +18,6 @@ app.config['MONGO_DBNAME'] = os.environ['MONGO_DBNAME']
 app.config['MONGO_USERNAME'] = os.environ['MONGO_USERNAME']
 app.config['MONGO_PASSWORD'] = os.environ['MONGO_PASSWORD']
 mongo = PyMongo(app)
-'''
 
 @app.route('/')
 def home():
